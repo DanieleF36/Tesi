64f448dd92a28a156fce000213dd9d03f5852b75
DanieleF36 <daniele.femia@outlook.com>
2024-11-05 21:18:44 +0100

    changed name from Monodirectional

--- TesiBackEnd/src/main/kotlin/esample/calcio/link/LinkUnidirectional.kt
+++ TesiBackEnd/src/main/kotlin/esample/calcio/link/LinkUnidirectional.kt
@@ -10,7 +10,7 @@ import conceptualMap2.event.PropagatedEvent
  * @param weight is a function that act on event statistics updating them according some criteria
  * @param filter all the event that must be propagated or not. Return true if the event is filtered and must be stopped from propagation
  */
-class LinkMonodirectional(
+class LinkUnidirectional(
     a: ConceptualMap,
     val weight: (event: Event) -> PropagatedEvent,
     val filter: (event: Event) -> Boolean
@@ -22,7 +22,7 @@ class LinkMonodirectional(
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (other !is LinkMonodirectional) return false
+        if (other !is LinkUnidirectional) return false
         return a != other.a
     }
 
