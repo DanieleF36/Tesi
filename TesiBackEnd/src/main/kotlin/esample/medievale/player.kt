4aa30e610a058627f8e582c22ff45fbd6810536f
DanieleF36 <daniele.femia@outlook.com>
2024-11-12 14:13:27 +0100

    added special object for player

--- TesiBackEnd/src/main/kotlin/esample/medievale/player.kt
+++ TesiBackEnd/src/main/kotlin/esample/medievale/player.kt
@@ -1,6 +1,37 @@
 package esample.medievale
 
+import conceptualMap2.event.AbstractEvent
+import conceptualMap2.event.EventImportance
+import conceptualMap2.event.EventType
 import conceptualMap2.npc.NPC
+import conceptualMap2.npc.knowledge.Knowledge
+import esample.calcio.npc.footballer.context.SimpleContext
+import esample.medievale.realEsample.map.cities.faelwenConceptualMap
 
-//TODO
-lateinit var player: NPC
+val player: NPC = object : NPC(
+    _name = "player",
+    group = faelwenConceptualMap,
+    context = Knowledge(SimpleContext(""), SimpleContext(""), SimpleContext(""), SimpleContext(""))
+){
+    override fun initialize() {
+        println("USER")
+    }
+
+    override fun endConversation() {
+        println("USER")
+    }
+
+    override fun generateRandomEvent(npc: NPC, type: EventType, importance: EventImportance) {
+        println("USER")
+    }
+
+    override fun toMap(): Map<String, Any?> {
+        println("USER")
+        return mapOf()
+    }
+
+    override fun addEvent(event: AbstractEvent) {
+        println("USER")
+    }
+
+}
